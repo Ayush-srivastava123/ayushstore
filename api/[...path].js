56f8b23d25b1a5c8638b33b4e86ec4a5090e741d
Ayush-srivastava123 <ayush2003gzb@gmail.com>
2026-08-14 03:48:46 +0530

    Add catch-all Vercel API route for Express backend

---
+++ api/[...path].js
@@ -0,0 +1,10 @@
+import app from '../backend/src/app.js';
+import { connectDatabase } from '../backend/src/config/db.js';
+
+let connectionPromise;
+
+export default async function handler(req, res) {
+  if (!connectionPromise) connectionPromise = connectDatabase();
+  await connectionPromise;
+  return app(req, res);
+}
